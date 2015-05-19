0994980cc41037a37392c5c4661c95453b17a150
Ken Scambler <ken.scambler@rea-group.com>
2015-05-19 22:02:41 +1000

    Scala syntax help

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-yowcat
+Introduction to Category Theory workshop 
 ==========
 
 Exercises for my Category Theory workshop at YOW Lambda Jam 2015 in Brisbane
@@ -6,3 +6,11 @@ Exercises for my Category Theory workshop at YOW Lambda Jam 2015 in Brisbane
 The exercises can be found in `src/main/scala/kenbot/yowcat`.
 
 You can test your answers with `./sbt test`
+
+Make sure you run ./sbt test before the workshop -- the first time it runs, it will download the internet! 
+
+ScalaSyntaxHelp.scala explains some of the basic Scala concepts used or required by the examples, which may be of use. 
+
+Using the Scala REPL is highly recommended for working through the exercises, and exploring solutions.
+
+`./sbt console`
---
+++ ScalaSyntaxHelp.scala
@@ -0,0 +1,124 @@
+object ScalaSyntaxHelp {
+  // Lists
+  // 
+  // The traditional immutable cons list.
+  // 
+  // Creating a List:
+  List(1,2,3,4)
+
+  // Constructing the same list using the cons operator:
+  // (It binds to the right, prepending each element on the left)
+  1 :: 2 :: 3 :: 4 :: Nil
+
+  // Pattern matching on a list
+  List(1,2,3) match {
+    case Nil => ???
+    case head :: tail => ???
+  }
+
+  // Appending two lists
+  List(1,2,3) ::: List(4,5,6) 
+  // or
+  List(1,2,3) ++ List(4,5,6)
+
+
+
+  // Streams.
+  //
+  // We use streams a lot here, to represent sets.
+  //
+  // They are essentially a lazy (and possibly infinite) cons list, 
+  // similar in some ways to lists in Haskell.
+  // 
+  // Creating a stream:
+  Stream(1,2,3,4) 
+
+  // Constructing the same stream using the cons operator:
+  // (It binds to the right, prepending each element on the left)
+  1 #:: 2 #:: 3 #:: 4 #:: Stream.Empty
+
+  // Pattern matching on a stream:
+  Stream(1, 2, 3) match {
+    case Stream.Empty => ??? 
+    case head #:: tail => ???
+  }
+
+  // Appending 2 streams
+  Stream(1,2,3) #::: Stream(4,5,6)
+  // or 
+  Stream(1,2,3) ++ Stream(4,5,6)
+
+
+  // Combinators
+  //
+  // Most collections such as as lists and streams (and their shared supertype Seq) 
+  // have a similar set of combinators.
+
+  // map
+  List(1, 2, 3).map(_ + 1) // ==> List(2,3,4) 
+
+  // flatMap
+  List(1, 2, 3).flatMap(n => List(n,n)) // ==> List(1, 1, 2, 2, 3, 3)
+
+  // filter
+  List(1, 2, 3).filter(_ > 1) // ==> List(2, 3)
+
+  // collect
+  // Collect uses a pattern matching block to simultaneously map and filter:
+  List(1, 2, 3).collect {
+    case 1 => "One" 
+    case 2 => "Two" 
+  }
+  // ==> List("One", "Two")
+
+  // fold
+  List(1, 2, 3).foldLeft(0)(_ + _) // ==> 6 
+
+  // For comprehensions
+  //
+  // If a class has flatMap and map methods, a for-comprehension can be used, similar to Haskell's do syntax:
+  for {
+    a <- List(1,2,3)
+    b <- List(a*2, a*3, a*4)
+    c <- List(b+1, b+2)
+  } yield (a, b, c)
+
+  // Which is syntax sugar for:
+  List(1,2,3).flatMap { a => 
+    List(a*2, a*3, a*4).flatMap { b => 
+      List(b+1, b+2).map { c => 
+        (a, b, c)
+      }
+    }
+  }
+
+  // Apply methods
+  //
+  // If an object offers a method called "apply", it can be called with function-like syntax.
+  List(1,2,3).apply(2) // ==> 3
+  List(1,2,3)(2) // ==> 3
+
+
+  // Type members
+  //
+  // Classes can have type members, just like fields or methods. 
+  // Type members can be abstract.
+  // The type belongs to instances of that class.
+  //
+  // Any fixed instance 'x' has a singleton type 'x.type'
+  trait Outer {
+    type Foo
+    def foo: Foo
+  }
+
+  val a: Outer = ??? 
+  val b: Outer = ???
+
+  // a.Foo is "The type 'Foo', belonging to the fixed instance 'a'"
+  val foo1: a.Foo = a.foo
+  //val foo2: a.Foo = b.foo // WON'T COMPILE!  b.foo is a b.Foo, not an a.Foo!
+
+  val onlyA: a.type = a
+  //val onlyA2: a.type = b // WON'T COMPILE! 'a.type' only allows the single instance 'a'. 
+
+}
